e82e1fce8cd29a887ca84454ecbf830abb321cb4
Akrame-Amine Dridi <aadridi@gmail.com>
2025-11-09 17:15:53 +0100

    Playing with custom validation of email in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles.css';
+
+const emailErrorsDisplay = document.querySelector('.emailErrorContainer');
+const emailInput = document.querySelector('#email');
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	console.log('Form is being checked!');
+	const email = form.email.value.trim();
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (!emailRegex.test(email)) {
+		emailInput.setCustomValidity(' ');
+		const p = document.createElement('p');
+		p.textContent = 'Adresse email invalide!';
+		emailErrorsDisplay.append(p);
+		return;
+	}
+	console.log('Formulaire validé');
+});
